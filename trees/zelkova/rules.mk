647fb5e2bc6a94e8d74cec6b8be747c48fd3e871
Nicholas Christiansen <nich.h.chris@gmail.com>
2023-07-02 17:02:41 +0200

    fix stuff

--- trees/zelkova/rules.mk
+++ trees/zelkova/rules.mk
@@ -1,15 +1,4 @@
-# This file intentionally left blank
-
-### Some potentially needed stuff taken from
-### https://github.com/GEIGEIGEIST/qmk-config-totem/blob/main/totem/rules.mk
-# Ignore some warnings during the build, likely to be fixed before RP2040 PR is merged
-ALLOW_WARNINGS = yes
-
-### needed?
-# # PIO serial/WS2812 drivers must be used on RP2040
-# SERIAL_DRIVER = vendor
-# WS2812_DRIVER = vendor
-
+SERIAL_DRIVER = vendor
 #SPACE SAVING
 AUDIO_ENABLE = no
 MUSIC_ENABLE = no
@@ -18,3 +7,6 @@ LTO_ENABLE = no
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no 
 MAGIC_ENABLE = no
+
+COMBO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
